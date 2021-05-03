bc912cd554e6232b0f22624af3e3930a510e661f
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-03 20:56:28 -0300

    added colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  white: '#FFF',
+  purple: '#59429d',
+  purpleLight: '#7030a0',
+  purpleDark: '#550073',
+  gray: '#e0e0f8',
+  shadowBox: 'rgba(0, 0, 0, 0.2)',
+  shadowText: 'rgba(0, 0, 0, 0.5)',
+  shadowLight: 'rgba(255, 255, 255, 0.8)',
+};
