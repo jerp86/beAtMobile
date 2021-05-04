4a7b13041e0966b8f8a8aabc3bfe90522f209f3f
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-03 23:31:30 -0300

    configuration Fonts loader

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 
 import { Welcome } from './src/screens/Welcome';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <Welcome />
   );
