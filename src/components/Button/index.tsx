a80b87526e252dc87cd73c2cbd5c800f10dd7906
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-04 01:17:22 -0300

    added Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ButtonText, Container } from './styles';
+
+export const Button = () => {
+  return (
+    <Container>
+      <ButtonText>
+        Confirmar
+      </ButtonText>
+    </Container>
+  );
+};
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { colors, fonts } from '../../styles';
+import { px } from '../../utils';
+
+export const Container = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.purpleLight};
+  height: ${px(56)}px;
+  border-radius: ${px(16)}px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: ${fonts.heading};
+  font-size: ${16}px;
+  color: ${colors.white};
+`;
