b0496027b4b9515f417f9df5244fb758daaf44f0
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-06 02:24:43 -0300

    added set AsyncStorage in UserIdentification and get in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { Container, Greeting, ImageHeader, UserName } from './styles';
 
 export const Header = () => {
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    const loadStorageUserName = async () => {
+      const user = await AsyncStorage.getItem('@beAt:user');
+      setUserName(user || '');
+    };
+
+    loadStorageUserName();
+  }, []);
+
   return (
     <Container>
       <View>
         <Greeting>Olá,</Greeting>
-        <UserName>Jerp</UserName>
+        <UserName>{userName}</UserName>
       </View>
 
       <ImageHeader
