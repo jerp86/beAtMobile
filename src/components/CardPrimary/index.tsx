f330a85060f68aa151dafa2d666aa1fb622eac22
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-06 22:08:43 -0300

    updated length to title

--- src/components/CardPrimary/index.tsx
+++ src/components/CardPrimary/index.tsx
@@ -14,7 +14,7 @@ export const CardPrimary = ({ data, ...rest }: QuestionnaireProps) => {
   return (
     <Container {...rest}>
       <Title>
-        {data.title.length < 50 ? data.title : `${data.title.substr(0, 50)}...`}
+        {data.title.length < 30 ? data.title : `${data.title.substr(0, 30)}...`}
       </Title>
 
       <Text>{`Há ${data.questionsId?.length ?? 0} perguntas`}</Text>
