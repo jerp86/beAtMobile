8f83ef7161f4fea0bb43733c2953a001d8e6667c
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-05-06 22:10:26 -0300

    updated styles to the QuestionAnswer

--- src/screens/QuestionAnswer/styled.ts
+++ src/screens/QuestionAnswer/styled.ts
@@ -1,29 +1,22 @@
 import styled from 'styled-components/native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { colors, fonts } from '../../styles';
 import { px } from '../../utils';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: space-around;
+  justify-content: space-between;
   background-color: ${colors.gray};
 `;
 
 export const Info = styled.View`
-  flex: 1;
   align-items: center;
   justify-content: center;
-  padding-horizontal: ${px(30)}px;
-  padding-vertical: ${px(50)}px;
+  padding-horizontal: ${px(16)}px;
+  padding-top: ${px(48)}px;
+  padding-bottom: ${px(8)}px;
   background-color: ${colors.gray};
-`;
-
-export const Controller = styled.View`
-  background-color: ${colors.white};
-  padding-horizontal: ${px(20)}px;
-  padding-top: ${px(20)}px;
-  padding-bottom: ${getBottomSpace() || px(20)}px;
 `;
 
 export const Name = styled.Text`
@@ -41,34 +34,21 @@ export const About = styled.Text`
   margin-top: ${px(8)}px;
 `;
 
-export const TipContainer = styled.View`
+export const TipContainer = styled(RectButton)`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  background-color: ${colors.purpleLight};
-  padding: ${px(20)}px;
+  background-color: ${colors.purple};
+  margin-bottom: ${px(8)}px;
+  padding: ${px(8)}px;
   border-radius: ${px(20)}px;
-  position: relative;
-  bottom: ${px(60)}px;
-`;
-
-export const TipImage = styled.Image`
-  width: ${px(56)}px;
-  height: ${px(56)}px;
 `;
 
 export const TipText = styled.Text`
   flex: 1;
-  color: ${colors.gray};
+  color: ${colors.white};
   font-family: ${fonts.text};
   font-size: ${px(17)}px;
   text-align: justify;
-  margin-left: ${px(20)}px;
-`;
-
-export const AlertLabel = styled.Text`
-  color: ${colors.purple};
-  font-family: ${fonts.complement};
-  font-size: ${px(12)}px;
-  margin-bottom: ${px(5)}px;
+  margin-horizontal: ${px(8)}px;
 `;
